2d75f0a4a0ae754b5d66bf240b855e3626d15ac5
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-14 23:34:59 +0530

    updated aba_end.cpp

--- Automata/End/aba_end.cpp
+++ Automata/End/aba_end.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 #include <string>
 #include "../../boilerplate/isvalid.hpp"
 using namespace std;
@@ -22,51 +23,83 @@ using namespace std;
 //     }
 // }
 
+// class DFA {
+// private:
+//     unordered_map<string, int> transition;
+//     int curr_state=0;
+//     int substring_len;
+//     string x;
+//     char sec;
+
+// public:
+
+//     void display(){
+
+//         for (const auto& x : transition){
+//             cout << x.first << "->" << x.second << endl;
+//         }
+
+//     }
+//     DFA(const string& sub) {
+//         substring_len = sub.length();
+//         for (int i = 0; i < substring_len; i++) {
+//             string state_str = to_string(i);
+//             transition[state_str + "a"] = (i < substring_len && sub[i] == 'a') ? i + 1 : 0;
+//             transition[state_str + "b"] = (i < substring_len && sub[i] == 'b') ? i + 1 : 0;
+//         }
+//     }
+
+//     bool accepts(const string& inp) {
+//         curr_state = 0;
+//         for (char ch: inp){
+//             string curr = to_string(curr_state);
+//             if(transition.find(string(curr+ch)) == transition.end()){
+//                 curr_state = 0;
+//             }
+
+//             else{
+//                 curr_state = transition[string(curr+ch)];
+
+//                 if(curr_state == substring_len){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
 class DFA {
 private:
-    unordered_map<string, int> transition;
-    int curr_state=0;
+    vector<vector<int>> transition;
+    int curr_state = 0;
     int substring_len;
-    string x;
-    char sec;
-
+    
 public:
-
-    void display(){
-
-        for (const auto& x : transition){
-            cout << x.first << "->" << x.second << endl;
-        }
-
-    }
     DFA(const string& sub) {
         substring_len = sub.length();
+
+        transition.assign(substring_len + 1, vector<int>(2, 0));
+
         for (int i = 0; i < substring_len; i++) {
-            string state_str = to_string(i);
-            transition[state_str + "a"] = (i < substring_len && sub[i] == 'a') ? i + 1 : 0;
-            transition[state_str + "b"] = (i < substring_len && sub[i] == 'b') ? i + 1 : 0;
+            if (i < substring_len) {
+                transition[i][sub[i] - 'a'] = i + 1;
+            }
         }
     }
 
     bool accepts(const string& inp) {
         curr_state = 0;
-        for (char ch: inp){
-            string curr = to_string(curr_state);
-            if(transition.find(string(curr+ch)) == transition.end()){
-                curr_state = 0;
+        for (char ch : inp) {
+            if (curr_state == substring_len) {
+                return true;
             }
-
-            else{
-                curr_state = transition[string(curr+ch)];
-
-                if(curr_state == substring_len){
-                    return true;
-                }
-            }
+            curr_state = transition[curr_state][ch - 'a'];
         }
-        return false;
+        return curr_state == substring_len;
     }
 };
+
 
 int main() {
     string input_s, sub;
