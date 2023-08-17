149a2ed960cf04b6f20738c25d244fe1a82d5950
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-17 23:03:20 +0530

    changing stuff up...

--- Algorithms/Merge Sort/m_sort.cpp
+++ Algorithms/Merge Sort/m_sort.cpp
@@ -4,26 +4,9 @@ using namespace std;
 void m_sort(int A[], int m, int B[], int n, int C[]){
     int i=0, j=0, k=0;
 
-    while(k < m+n){
-        if(i < n || A[i]<=B[j]){
-            C[k] = A[i]; i++; k++;
-        }
+    while(i<m && j<n){
 
-        else if(j < m || B[j]<=A[i]){
-            C[k] = B[j]; j++; k++;
-        }
     }
-
-    cout << "Array after Merge Sort" << endl;
-
-    for(int x=0; x < k+1; x++){
-        cout << C[x] << "\t";
-    }
-
-    delete[] A;
-    delete[] C;
-    delete[] A;
-    delete[] B;
 }
 
 int main(){
@@ -32,25 +15,40 @@ int main(){
     cin >> size;
 
     int* arr = new int(size);
-    int* C = new int(size);
 
     int m = size/2;
     int n = size - m;
 
     int* A = new int[m];
     int* B = new int[n];
+    int* C = new int[size];
 
     for(int i=0; i<size; i++){
         cout << "Enter element at index position " << i << " : ";
         cin >> arr[i];
     }
 
+    for(int i=0; i<m; i++){
+        A[i] = arr[i];
+    }
+
+    for(int i=0; i<n; i++){
+        B[i] = arr[m+i];
+    }
+
     cout << "Array before Merge Sort" << endl;
 
     for(int i=0; i<size; i++){
-        cout << C[i] << "\t";
+        cout << arr[i] << "\t";
     }
 
+    cout << endl;
+
     m_sort(A, m, B, n, C);
+
+    delete[] arr;
+    delete[] A;
+    delete[] B;
+    delete[] C;
     
 }
