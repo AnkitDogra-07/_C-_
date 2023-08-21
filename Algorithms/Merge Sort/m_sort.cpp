8dce956f74aa8cccb8f50e9d6e9da957e5b0574f
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-21 22:46:01 +0530

    documented the merge_sort algorithm cpp file

--- Algorithms/Merge Sort/m_sort.cpp
+++ Algorithms/Merge Sort/m_sort.cpp
@@ -2,46 +2,58 @@
 #include<vector>
 using namespace std;
 
+// Function to merge two sorted arrays A and B into C.
 void merge(vector<int>& A, vector<int>& B, vector<int>& C){
     int m = A.size();
     int n = B.size();
-    int i=0, j=0, k=0;
+    int i = 0, j = 0, k = 0;
 
+    // Merge elements from A and B into C while maintaining sorted order.
     while(i < m && j < n){
-        if(A[i] <= B[i]){
-            C[k] = A[i]; i++;
+        if(A[i] <= B[j]){
+            C[k] = A[i]; 
+            i++;
         }
-
         else{
-            C[k] = B[j]; j++; 
+            C[k] = B[j]; 
+            j++; 
         }
         k++;
     }
 
+    // Copy remaining elements from A and B into C if any.
     while(i < m){
-        C[k] = A[i]; i++; k++;
+        C[k] = A[i]; 
+        i++; 
+        k++;
     }
 
     while(j < n){
-        C[k] = B[j]; j++; k++;
+        C[k] = B[j]; 
+        j++; 
+        k++;
     }
 }
 
+// Recursive function to perform merge sort on array A and store the result in B.
 void mergeSort(vector<int>& A, vector<int>& B, int left, int right){
     if(right - left <= 1){
-        return;
+        return; // Base case: A has one or zero elements, so it's already sorted.
     }
 
-    int mid = left + ((right - left)/2);
+    int mid = left + ((right - left) / 2);
+
+    // Create subarrays l and r to divide A into two halves.
     vector<int> l(A.begin() + left, A.begin() + mid);
     vector<int> r(A.begin() + mid, A.begin() + right);
 
+    // Recursively sort the subarrays.
     mergeSort(A, l, left, mid);
     mergeSort(A, r, mid, right);
 
+    // Merge the sorted subarrays l and r into B.
     merge(l, r, B);
 }
-
 
 int main() {
     int size;
@@ -51,6 +63,7 @@ int main() {
     vector<int> arr(size);
     vector<int> C(size);
 
+    // Input elements of the array.
     for (int i = 0; i < size; i++) {
         cout << "Enter element at index position " << i << " : ";
         cin >> arr[i];
@@ -58,16 +71,19 @@ int main() {
 
     cout << "Array before Merge Sort" << endl;
 
+    // Display the original array.
     for (int i = 0; i < size; i++) {
         cout << arr[i] << "\t";
     }
 
     cout << endl;
 
+    // Perform merge sort on the input array and store the sorted result in C.
     mergeSort(arr, C, 0, size);
 
     cout << "Array after Merge Sort" << endl;
 
+    // Display the sorted array.
     for (int i = 0; i < size; i++) {
         cout << C[i] << "\t";
     }
