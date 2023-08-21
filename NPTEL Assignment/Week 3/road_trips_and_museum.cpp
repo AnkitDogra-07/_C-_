a6a8aab6fdfd23751119c37127a6ca48eccaf28a
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-21 23:10:57 +0530

    Documented road_trips_and_museum.cpp
    This code performs a detailed analysis of cities, roads, and vacation days and calculates the maximum number of museums a person can visit based on different scenarios. It correctly utilizes depth-first search (DFS) to explore cities and count the museums that can be visited from each city.

--- NPTEL Assignment/Week 3/road_trips_and_museum.cpp
+++ NPTEL Assignment/Week 3/road_trips_and_museum.cpp
@@ -3,14 +3,15 @@
 #include<algorithm>
 using namespace std;
 
-int dfs(vector<vector<int>>& direct, vector<int>& visited, vector<int>& museum, int node, int ans){
+// Depth-First Search (DFS) function to explore cities and calculate museums to visit.
+int dfs(vector<vector<int>>& direct, vector<int>& visited, vector<int>& museum, int node, int ans) {
     visited[node] = 1;
 
-    // Add the number of museums in the current node to ans
+    // Add the number of museums in the current city to ans.
     ans += museum[node];
 
-    for(int i : direct[node]){
-        if(visited[i] == 0){
+    for (int i : direct[node]) {
+        if (visited[i] == 0) {
             ans = dfs(direct, visited, museum, i, ans);
         }
     }
@@ -18,69 +19,67 @@ int dfs(vector<vector<int>>& direct, vector<int>& visited, vector<int>& museum, 
     return ans;
 }
 
-int main(){
-    int n, m, k, u, v, mu, cnt=0, sum=0;
+int main() {
+    int n, m, k, u, v, mu, cnt = 0, sum = 0;
 
-    //Entering number of cities, number of roads and number of months respectively...
+    // Input the number of cities, roads, and vacation days.
     cin >> n >> m >> k;
 
-    vector<vector<int>> direct(n+1); vector<int> museum(n+1); vector<int> total_museum;
-    vector<int> visited(n+1, 0);
+    vector<vector<int>> direct(n + 1); // Direct routes between cities.
+    vector<int> museum(n + 1); // Number of museums in each city.
+    vector<int> total_museum; // Stores museums count reachable from each city.
+    vector<int> visited(n + 1, 0); // Marks visited cities.
 
-    //Entering all the direct routes...
-    for(int i=1; i<m+1; i++){
+    // Input information about direct routes between cities.
+    for (int i = 1; i <= m; i++) {
         cin >> u >> v;
         direct[u].push_back(v);
         direct[v].push_back(u);
     }
 
-    //Entering all the musuem in ith city at the corresponding index position i...
-    for(int i=1; i<n+1; i++){
+    // Input the number of museums in each city.
+    for (int i = 1; i <= n; i++) {
         cin >> mu;
         museum[i] = mu;
     }
 
-    for(int i=1; i<n+1; i++){
-        if(visited[i] == 0){
-            cnt++;
+    // Iterate through cities to calculate reachable museums.
+    for (int i = 1; i <= n; i++) {
+        if (visited[i] == 0) {
+            cnt++; // Count unique cities visited.
             int rec = dfs(direct, visited, museum, i, 0);
             total_museum.push_back(rec);
         }
     }
 
-    sort(total_museum.begin(), total_museum.end());
+    sort(total_museum.begin(), total_museum.end()); // Sort museums count.
 
-    //When number of cities to visit are less than number of holidays...
-    if(cnt < k){
+    // When the number of cities to visit is less than the number of holidays.
+    if (cnt < k) {
         cout << -1;
     }
-
-    //When number of cities to visit are same as number of holidays...
-    else if(cnt == k){
-        for(int i:total_museum){
-            sum+=i;
+    // When the number of cities to visit is equal to the number of holidays.
+    else if (cnt == k) {
+        for (int i : total_museum) {
+            sum += i;
         }
-
         cout << sum;
     }
+    // When the number of cities to visit is more than the number of holidays.
+    else if (cnt > k) {
+        int i = 0, j = total_museum.size() - 1;
 
-    //When number of cities to visit are more than number of holidays...
-    else if (cnt > k){
-        int i=0, j=total_museum.size()-1;
-    
-        while(k){
-            sum+=total_museum[j--];
+        while (k) {
+            sum += total_museum[j--];
             k--;
 
-            if(k){
-                sum+=total_museum[i++];
+            if (k) {
+                sum += total_museum[i++];
                 k--;
             }
         }
-
         cout << sum;
-
     }
-    
+
     return 0;
 }
