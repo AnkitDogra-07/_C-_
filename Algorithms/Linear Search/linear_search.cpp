f4cb993f698f1d51234b54cb75b23136937e35cc
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-21 22:49:08 +0530

    Documented linear_search.cpp

--- Algorithms/Linear Search/linear_search.cpp
+++ Algorithms/Linear Search/linear_search.cpp
@@ -1,36 +1,37 @@
 #include<iostream>
 using namespace std;
 
-bool b_search(int A[], int n, int k){
+// Function to perform linear search for an element 'k' in an array 'A' of size 'n'.
+bool b_search(int A[], int n, int k) {
+    int flag = 0; // Initialize a flag variable to 0.
 
-    int flag = 0; 
-    for(int i=0; i<n; i++){
-        if(k == A[i]){
-            flag = 1;
-            
+    // Loop through the array to check if 'k' is equal to any element in 'A'.
+    for (int i = 0; i < n; i++) {
+        if (k == A[i]) {
+            flag = 1; // Set the flag to 1 if 'k' is found in 'A'.
         }
     }
-    
-    //flag to check if element is found.
-    if(flag == 1){
-        return true;
+
+    // Check the flag to determine if 'k' was found in the array.
+    if (flag == 1) {
+        return true; // 'k' is found in the array, return true.
     }
 
-    return false;
-
+    return false; // 'k' is not found in the array, return false.
 }
 
-int main(){
-    int arr[5] = {1,2,3,4,5};
-    int k  = 7;
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+    int k = 7;
 
+    // Call the b_search function to search for 'k' in 'arr'.
     bool ans = b_search(arr, 5, 7);
 
-    if(ans == true){
-        cout<<k<<" is in the array.";
+    if (ans == true) {
+        cout << k << " is in the array.";
+    } else {
+        cout << k << " is not in the array.";
     }
 
-    else{
-        cout<<k<<" is not in the array.";
-    }
+    return 0;
 }
