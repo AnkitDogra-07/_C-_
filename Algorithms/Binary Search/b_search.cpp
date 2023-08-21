5b40a05fbde3736979a20ffd3c23b8ce9c618d7e
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-21 22:52:06 +0530

    Documented b_search.cpp
    This code allows you to perform both iterative and recursive binary searches on a sorted array. The user can choose the search method interactively. The comments provide explanations for each part of the code.

--- Algorithms/Binary Search/b_search.cpp
+++ Algorithms/Binary Search/b_search.cpp
@@ -1,91 +1,85 @@
 #include<iostream>
 using namespace std;
 
-int b_search(int A[], int l, int r, int key){ //Iterative Binary Search...
-    
-    while(l <= r){
-        int mid = l + (r-l)/2;
+// Function to perform Iterative Binary Search in a sorted array 'A' between indices 'l' and 'r' for a given 'key'.
+int b_search(int A[], int l, int r, int key) {
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
 
-        if(key == A[mid]){
-            return mid;
+        if (key == A[mid]) {
+            return mid; // Key found at index 'mid'.
         }
 
-        if(key < A[mid]){
-            r = mid - 1;
-        }
-
-        else{
-            l = mid + 1;
+        if (key < A[mid]) {
+            r = mid - 1; // Adjust the search range to the left half.
+        } else {
+            l = mid + 1; // Adjust the search range to the right half.
         }
     }
 
-    return -1;
-
+    return -1; // Key not found in the array.
 }
 
-int b_search_rec(int A[], int l, int r, int key){ //Recurisve Binary Search...
+// Function to perform Recursive Binary Search in a sorted array 'A' between indices 'l' and 'r' for a given 'key'.
+int b_search_rec(int A[], int l, int r, int key) {
+    if (l <= r) {
+        int mid = l + (r - l) / 2;
 
-    if(l <= r){
-        int mid = l + (r-l)/2;
-
-        if(key == A[mid]){
-            return mid;
+        if (key == A[mid]) {
+            return mid; // Key found at index 'mid'.
         }
 
-        if(key < A[mid]){
-            return b_search_rec(A, l, mid-1, key);
+        if (key < A[mid]) {
+            return b_search_rec(A, l, mid - 1, key); // Recursively search the left half.
+        } else {
+            return b_search_rec(A, mid + 1, r, key); // Recursively search the right half.
         }
-
-        return b_search_rec(A, mid+1, r, key);
     }
 
-    return -1;
+    return -1; // Key not found in the array.
 }
 
-int main(){
+int main() {
     int size;
-    cout << "Enter size of array:";
+    cout << "Enter size of array: ";
     cin >> size;
 
     int* arr = new int[size];
 
+    // Input elements of the sorted array.
     for (int i = 0; i < size; i++) {
-        cout << "Enter element at position " << i << ":";
+        cout << "Enter element at position " << i << ": ";
         cin >> arr[i];
     }
 
-    cout << endl;
-
     int ans;
     int x;
-    cout << "Enter number to search:";
+    cout << "Enter number to search: ";
     cin >> x;
     cout << endl;
 
     cout << "Enter" << std::endl
-              << "(1) to use Binary Search Iteratively" << std::endl
-              << "(2) to use Binary Search Recursively" << std::endl;
+        << "(1) to use Binary Search Iteratively" << std::endl
+        << "(2) to use Binary Search Recursively" << std::endl;
 
     int choose;
     cin >> choose;
 
     if (choose == 1) {
-        ans = b_search(arr, 0, size-1, x);
+        ans = b_search(arr, 0, size - 1, x); // Call the iterative binary search function.
     } else if (choose == 2) {
-        ans = b_search_rec(arr, 0, size-1, x);
+        ans = b_search_rec(arr, 0, size - 1, x); // Call the recursive binary search function.
     } else {
         cout << "Invalid choice. Please enter 1 or 2.\n";
     }
 
-    if(ans != -1){
+    if (ans != -1) {
         cout << x << " is present at index position " << ans;
+    } else {
+        cout << x << " is not present in the array.";
     }
 
-    else{
-        cout << x << " is not present in array.";
-    }
-
-    delete[] arr;
+    delete[] arr; // Deallocate the dynamically allocated memory.
 
     return 0;
 }
