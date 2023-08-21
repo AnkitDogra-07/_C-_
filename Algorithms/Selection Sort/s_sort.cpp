9699430e667a76557b220a8aeebd76bf45283206
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-21 22:55:37 +0530

    Documented s_sort.cpp
    This code demonstrates two versions of the selection sort algorithm: an iterative version and a recursive version. The user can choose the sorting method interactively. The comments provide explanations for each part of the code.

--- Algorithms/Selection Sort/s_sort.cpp
+++ Algorithms/Selection Sort/s_sort.cpp
@@ -1,51 +1,57 @@
 #include<iostream>
-#include "../Boilerplate/swap.hpp"
+#include "../Boilerplate/swap.hpp" // Include a custom swap header if available
 
-void selection_sort(int A[], int n){
-    for(int i=0; i<n; i++){
+// Function to perform Iterative Selection Sort on an array 'A' of size 'n'.
+void selection_sort(int A[], int n) {
+    for (int i = 0; i < n; i++) {
         int key = i;
-        for(int j=i+1; j<n; j++){
-            if(A[j] < A[key]){
+        // Find the index of the minimum element in the unsorted part of the array.
+        for (int j = i + 1; j < n; j++) {
+            if (A[j] < A[key]) {
                 key = j;
             }
         }
 
-        if(key != i){
+        // Swap the minimum element with the current element.
+        if (key != i) {
             swap(A[i], A[key]);
         }
     }
 }
 
-void selection_sort_rec(int A[], int start, int n){
-    
+// Function to perform Recursive Selection Sort on an array 'A' from 'start' to 'n'.
+void selection_sort_rec(int A[], int start, int n) {
     int key = start;
-    int j = start+1;
+    int j = start + 1;
 
-    if(start >= n-1){
-        return;
+    if (start >= n - 1) {
+        return; // Base case: The entire array is sorted.
     }
 
-    while(j < n){
-        if(A[j] < A[key]){
+    // Find the index of the minimum element in the unsorted part of the array.
+    while (j < n) {
+        if (A[j] < A[key]) {
             key = j;
         }
         j++;
     }
 
-    if(key != start){
+    // Swap the minimum element with the element at 'start'.
+    if (key != start) {
         swap(A[key], A[start]);
     }
 
-    return selection_sort_rec(A, start+1, n);
-
+    // Recursively sort the rest of the array.
+    return selection_sort_rec(A, start + 1, n);
 }
 
 int main() {
     int size;
     std::cout << "Enter length of array: ";
     std::cin >> size;
-    int* arr = new int[size];
+    int* arr = new int[size]; // Allocate dynamic memory for the array.
 
+    // Input elements of the array.
     for (int i = 0; i < size; i++) {
         std::cin >> arr[i];
     }
@@ -58,9 +64,9 @@ int main() {
     std::cin >> choose;
 
     if (choose == 1) {
-        selection_sort(arr, size);
+        selection_sort(arr, size); // Call the iterative selection sort function.
     } else if (choose == 2) {
-        selection_sort_rec(arr, 0, size);
+        selection_sort_rec(arr, 0, size); // Call the recursive selection sort function.
     } else {
         std::cout << "Invalid choice. Please enter 1 or 2.\n";
     }
@@ -71,7 +77,7 @@ int main() {
     }
     std::cout << "\n";
 
-    delete[] arr;
+    delete[] arr; // Deallocate the dynamically allocated memory.
 
     return 0;
 }
