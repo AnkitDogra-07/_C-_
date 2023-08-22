2831317cc74d0b3a72fdff0a02fc1a8b5672e4ea
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-22 22:49:17 +0530

    Update q_sort.cpp

--- Algorithms/Quick Sort/q_sort.cpp
+++ Algorithms/Quick Sort/q_sort.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 #include<vector>
-#include"../Boilerplate/swap.hpp"
+#include<algorithm>
 using namespace std;
 
-void partition(vector<int>& A, int low, int high){
+int partition(vector<int>& A, int low, int high){
     int pivot = A[high];
+
+    int i = low - 1;
+
+    for(int j=low; j<high; j++){
+        if(A[j] <= pivot){
+            i++;
+            swap(A[i], A[j]);
+        }
+    }
+
+    swap(A[i+1], A[high]);
+
+    return (i+1);
 }
+
+void quick_sort()
