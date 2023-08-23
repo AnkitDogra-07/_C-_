2c54bf074098b77cf7064d9f899fbdd56fb70643
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-24 00:00:23 +0530

    updated number_trips.cpp

--- NPTEL Assignment/Week 4/Number_Tripes.cpp
+++ NPTEL Assignment/Week 4/Number_Tripes.cpp
@@ -1,27 +1,56 @@
-#include<iostream>
-#include<vector>
-#include<string>
+#include <iostream>
+#include <vector>
+#include <map>
 using namespace std;
 
-struct Edge{
+struct Edge {
     int vertex;
     int weight;
 };
 
-int main(){
-    int M, A, B, u, w, v;
+int main() {
+    int M, A, B;
 
+    map<int, int> vertexMapping;
     cin >> M >> A >> B;
 
-    vector<vector<Edge>> chain;
+    // Initialize an empty vector of vectors
+    vector<vector<Edge>> chain(M);
+    int uniqueIndex = 0;
 
-    for(int i=0; i<M; i++){
-        for(int j=0; j<3; j++){
-           cin >>  u >> w >> v;
-           chain[u].push_back({v, w});
-           chain[v].push_back({u, w});
+
+    for (int i = 0; i < M; i++) {
+        int u, w, v;
+        cin >> u >> w >> v;
+
+        if(vertexMapping.find(u) == vertexMapping.end()){
+            vertexMapping[u] = uniqueIndex++;
+        }
+
+        if(vertexMapping.find(v) == vertexMapping.end()){
+            vertexMapping[v] = uniqueIndex++;
+        }
+
+        if(max(vertexMapping[u], vertexMapping[v]) >= chain.size()){
+            chain.resize(max(vertexMapping[u], vertexMapping[v]) + 1);
+        }
+
+        u = uniqueIndex;
+        v = uniqueIndex;
+
+        // Create edges and add them to the adjacency lists
+        chain[u].push_back({v, w});
+        chain[v].push_back({u, w});
+    }
+
+    // Print the adjacency lists
+    for (int i = 0; i < chain.size(); i++) {
+        cout << "Adjacency list of vertex " << i << ": ";
+        for (const Edge& edge : chain[i]) {
+            cout << "(" << edge.vertex << ", " << edge.weight << ") ";
         }
         cout << endl;
     }
 
+    return 0;
 }
