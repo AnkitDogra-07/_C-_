ba887d8a923da3a294a68b492d0499bc66cac81a
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-24 22:30:23 +0530

    Number_of_triples.cpp

--- NPTEL Assignment/Week 4/Number_Triples.cpp
+++ NPTEL Assignment/Week 4/Number_Triples.cpp
@@ -5,49 +5,67 @@
 #include <unordered_map>
 using namespace std;
 
-struct Edge {
-    int vertex;
-    int weight;
-};
-
 int INF = numeric_limits<int>::max();
 
-void dijkstra(unordered_map<int, vector<Edge>> graph, int a, int b, int size){
+int dijkstra(vector<vector<pair<int, int>>>& graph, int a, int b, int size){
     vector<int> dist(size, INF);
     dist[a] = 0;
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({0, a});
+    pq.push({a, 0});
 
     while(!pq.empty()){
-        
+        int u = pq.top().first;
+        int d = pq.top().second;
+        pq.pop();
+        for(auto it: graph[u]){
+            int v = it.first;
+            int wt = it.second;
+
+            if(d + wt < dist[v]){
+                dist[v] = dist[u] + wt;
+                pq.push({v, dist[v]});
+            }
+        }
     }
+
+    return dist[b];
 }
 
 int main() {
     int M, A, B, u, w, v;
 
     cin >> M >> A >> B;
-    int unq_index = 0;
 
-    int max = 0;
+    int N = 0;
 
-    unordered_map<int, vector<Edge>> chain;
+    vector<vector<int>> edges;
 
     for(int i=0; i<M; i++){
         cin >> u >> w >> v;
-        chain[u].push_back({v, w});
-        chain[v].push_back({u, w});
-
-        if(u > v && u > max){
-            max = u;
-        }
-
-        if(v > u && v > max){
-            max = v;
-        }
+        edges.push_back({u, w, v});
+        N = max(N, max(u,v));
     }
 
+    vector<vector<pair<int, int>>> chain(N+1);
+    for(int i=0; i<M; i++){
+        int u = edges[i][0];
+        int v = edges[i][2];
+        int wt = edges[i][1];
+
+        chain[u].push_back({v, wt});
+        chain[v].push_back({u, wt});
+    }
+
+    int ans = dijkstra(chain, A, B, N);
+
+    if(ans != INF){
+        cout << "YES" << endl << ans;
+    }
+
+    else{
+        cout << "NO" << endl;
+    }
+
+    return 0;
 }
-
-
