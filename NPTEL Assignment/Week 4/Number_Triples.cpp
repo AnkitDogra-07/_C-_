3b457dce75d2b90bbddbf05d4810fe36be0cfa74
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-24 19:14:41 +0530

    Update Number_Triples.cpp

--- NPTEL Assignment/Week 4/Number_Triples.cpp
+++ NPTEL Assignment/Week 4/Number_Triples.cpp
@@ -12,14 +12,25 @@ struct Edge {
 
 int INF = numeric_limits<int>::max();
 
-void dijkstra(){
+void dijkstra(unordered_map<int, vector<Edge>> graph, int a, int b, int size){
+    vector<int> dist(size, INF);
+    dist[a] = 0;
 
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, a});
+
+    while(!pq.empty()){
+        
+    }
 }
 
 int main() {
     int M, A, B, u, w, v;
 
     cin >> M >> A >> B;
+    int unq_index = 0;
+
+    int max = 0;
 
     unordered_map<int, vector<Edge>> chain;
 
@@ -27,6 +38,14 @@ int main() {
         cin >> u >> w >> v;
         chain[u].push_back({v, w});
         chain[v].push_back({u, w});
+
+        if(u > v && u > max){
+            max = u;
+        }
+
+        if(v > u && v > max){
+            max = v;
+        }
     }
 
 }
