79ced38450b336f228c38997430e166bf126811f
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2023-08-31 22:39:20 +0530

    Update AMBIDEX.cpp

--- CodeChef/I can use both hands/AMBIDEX.cpp
+++ CodeChef/I can use both hands/AMBIDEX.cpp
@@ -1,71 +1,28 @@
 #include <iostream>
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <algorithm>
-#include <random>
-
 using namespace std;
 
 int main() {
-    string cmp = "codechef";
-    unordered_map<char, vector<int>> map;
-    int v = 0;
-    string S;
-    for (const auto& x : cmp) {
-        map[x].push_back(v);
-        v++;
-    }
+    int T;
+    cin >> T;
 
-    int t;
-    cin >> t;
+    while (T--) {
+        int L, R, M;
+        cin >> L >> R >> M;
 
-    while (t--) {
-        vector<bool> used(8, false); 
-        string s(S.length(), ' ');
-        cin >> S;
-        sort(S.begin(), S.end());
+        int pages = 0, codeLines = 0;
 
-        for (const char& x : S) {
-            if (map.find(x) != map.end()) {
-                for (const auto& i : map[x]) {
-                    int pos = (i + 1) % 8;
-                    while (used[pos]) {
-                        pos = (pos + 1) % 8;
-                    }
-                    s[pos] = x;
-                    used[pos] = true;
-                    cout << "map" << endl;
-                }
-            }
-
-            else {
-                vector<int> available_positions;
-                for (int i = 0; i < 8; i++) {
-                    if (!used[i]) {
-                        available_positions.push_back(i);
-                    }
-                    cout << "available positions" << endl;
-                }
-
-                if (!available_positions.empty()) {
-                    // Generate a random index from the available positions
-                    random_device rd;
-                    mt19937 gen(rd());
-                    uniform_int_distribution<int> dist(0, available_positions.size() - 1);
-                    int random_index = dist(gen);
-
-                    // Place x at the randomly selected position
-                    int j = available_positions[random_index];
-                    s[j] = x;
-                    used[j] = true;
-                    cout << x << endl;
-                }
-            }
-            cout << "outer" << endl;
+        if(M%L == 0){
+            pages = M/L;
         }
+        
+        else{
+            pages = M/L + 1;
+        }
+        
+        codeLines = M/R;
 
-        cout << s << endl;
+        cout << pages + codeLines << endl;
     }
+
     return 0;
 }
