fe8ff62723efbb1505c924ae9e14ef8d5362c0be
Ankit Dogra <ankit.dogra2002@gmail.com>
2023-09-04 22:50:52 +0530

    Update knapsack.cpp

--- Algorithms/Knapsack/knapsack.cpp
+++ Algorithms/Knapsack/knapsack.cpp
@@ -8,12 +8,17 @@ struct item{
 };
 
 int knapSack(vector<item>& items, int capacity){
- int n = items.size();
- vector<vector<int>> dp(n, vector<int>(capacity+1, 0));
- 
- for(int i=0; i<n; i++){
-  for(int w=0; w<=capacity; w++){
-   
+  int n = items.size();
+  vector<vector<int>> dp(n, vector<int>(capacity+1, 0));
+  
+  for(int i=0; i<n; i++){
+    for(int w=0; w<=capacity; w++){
+      if(dp[i] <= w){
+        dp[i][w] = max(
+          (i>0 ? dp[i-1][w] : 0),
+          (i>0 ? dp[i-1][w - items[i].weight] : 0) + items[i].profit
+        );  
+      }
+    }
   }
- }
 }
