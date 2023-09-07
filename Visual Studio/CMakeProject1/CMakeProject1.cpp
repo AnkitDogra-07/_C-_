846e33651bc6b5aabcbcb2ae1eefaf8151dfd7f2
Ankit Dogra <ankit.dogra2002@gmail.com>
2023-09-07 16:49:44 +0530

    some minor changes

---
+++ Visual Studio/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	vector<int> arr(10, 0);
+	for (int i = 0; i < 10; i++) {
+		cin >> arr[i];
+	}
+}
---
+++ Visual Studio/CMakeProject1/CMakeProject1.h
@@ -0,0 +1,9 @@
+﻿// CMakeProject1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+// TODO: Reference additional headers your program requires here.
