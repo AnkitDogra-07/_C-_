fe9b85eaed75e59139b6dfddb7bbf8c547d8ba71
Ankit Dogra <ankit.dogra2002@gmail.com>
2023-09-12 09:37:30 +0530

    Create bfs.cpp

---
+++ Algorithms/Bredth First Search/bfs.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<unordered_map>
+#include<string>
+using  std::string, std::cout, std::cin, std::endl;
+
+unordered_map<int, vector<int>> adj_lst;
+
+void search(curr_node, vector<bool>& visited, max){
+  visited(max, False);
+  list<int> q;
+
+  visited[curr_node] = true;
+  q.push_back(curr_node);
+
+  while(!q.empty()){
+    int s = q.first();
+    q.pop();
+     	
+  }
+}
+  
