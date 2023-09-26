dc7015e00042e5f3bae8466e6de3dea46c8bf064
Ankit Dogra <ankit.dogra2002@gmail.com>
2023-09-26 10:10:14 +0530

    Update heap_sort.cpp

--- Algorithms/HeapSort/heap_sort.cpp
+++ Algorithms/HeapSort/heap_sort.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include <vector>
+#include "../Boilerplate/swap.hpp"
 using std::vector;
 
-int insert_node(vector<int>& heap, int node){
-  int n = heap.size();
+void heapify(vector<int>& heap, int i){
+  int parent = (i-1)/2;
   
+  if(heap[parent] > 0){
+    if(heap[parent] < heap[i]){
+      swap(heap[i], heap[parent]);
+
+      heapify(heap, parent);   
+    }
+  }
+}
+
+void insert_node(vector<int>& heap, int node){
+  vector<int> heap(heap.size() + 1);
+  heap[heap.size() - 1] = node;
+  heapify(heap, heap.size() - 1);
 }
 
 int main(){
