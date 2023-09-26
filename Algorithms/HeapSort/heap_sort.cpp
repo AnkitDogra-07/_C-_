66aaa986ac38c24b417888fd849fcc709d8fcb76
Ankit Dogra <ankit.dogra2002@gmail.com>
2023-09-26 10:49:55 +0530

    updates

--- Algorithms/HeapSort/heap_sort.cpp
+++ Algorithms/HeapSort/heap_sort.cpp
@@ -3,22 +3,31 @@
 #include "../Boilerplate/swap.hpp"
 using std::vector, std::cout, std::cin;
 
-void heapify(vector<int>& heap, int i){
+void heapify(vector<int>& heap, int n, int i){
   int parent = (i-1)/2;
-  
-  if(heap[parent] > 0){
-    if(heap[parent] < heap[i]){
-      swap(heap[i], heap[parent]);
+  int left = (2*i) - 1;
+  int right = (2*i) + 2;
 
-      heapify(heap, parent);   
-    }
+  if(left < n && heap[left] > heap[parent]){
+    parent = left;
+  }
+
+  if(right < n && heap[right] > heap[parent]){
+    parent = right;
+  }
+
+  if(parent != i){
+    swap(heap[parent], heap[i]);
+    heapify(heap, n, parent);
   }
 }
 
-void insert_node(vector<int>& heap, int node){
-  vector<int> heap(heap.size() + 1);
-  heap[heap.size() - 1] = node;
-  heapify(heap, heap.size() - 1);
+void build_heap(vector<int>& heap){
+  int index_init = (heap.size()/2) - 1;
+
+  for(int i= index_init; i>=0; i--){
+    heapify(heap, heap.size(), i);
+  }
 }
 
 int main(){
@@ -26,14 +35,16 @@ int main(){
   cout << "Enter size of heap : ";
   cin >> n;
 
-  vector<int> heap(n, 0);
+  vector<int> heap(n);
 
   for(int i=0; i<n; i++){
     cin >> node;
-    insert_node(heap, node);
+    heap.push_back(node);
   }
 
+  build_heap(heap);
+
   for(const int& x: heap){
-    cout << x;
+    cout << x << "\t";
   }
 }
