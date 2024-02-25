199a76ddaba134e608405e170270ae895075c658
AnkitDogra-07 <ankit.dogra2002@gmail.com>
2024-02-26 01:39:12 +0530

    new merge sort code

--- Algorithms/Merge Sort/m_sort.cpp
+++ Algorithms/Merge Sort/m_sort.cpp
@@ -2,57 +2,40 @@
 #include<vector>
 using namespace std;
 
-// Function to merge two sorted arrays A and B into C.
-void merge(vector<int>& A, vector<int>& B, vector<int>& C){
-    int m = A.size();
-    int n = B.size();
-    int i = 0, j = 0, k = 0;
+void merge(vector<int>& c, int l, int m, int r){
+    vector<int> temp;
+    int low = l;
+    int high = m+1;
 
-    // Merge elements from A and B into C while maintaining sorted order.
-    while(i < m && j < n){
-        if(A[i] <= B[j]){
-            C[k] = A[i]; 
-            i++;
+    while(low <= m && high <= r){
+        if(c[low] <= c[high]){
+            temp.push_back(c[low]);
+	    low++;
         }
         else{
-            C[k] = B[j]; 
-            j++; 
+            temp.push_back(c[high]);
+	    high++;
         }
-        k++;
     }
 
-    // Copy remaining elements from A and B into C if any.
-    while(i < m){
-        C[k] = A[i]; 
-        i++; 
-        k++;
+    while(low <= m){
+        temp.push_back(c[low++]);
     }
 
-    while(j < n){
-        C[k] = B[j]; 
-        j++; 
-        k++;
+    while(high <= r){
+        temp.push_back(c[high++]);
+    }
+    for(int i=l; i<=r; i++){
+        c[i] = temp[i-l];
     }
 }
 
-// Recursive function to perform merge sort on array A and store the result in B.
-void mergeSort(vector<int>& A, vector<int>& B, int left, int right){
-    if(right - left <= 1){
-        return; // Base case: A has one or zero elements, so it's already sorted.
-    }
-
-    int mid = left + ((right - left) / 2);
-
-    // Create subarrays l and r to divide A into two halves.
-    vector<int> l(A.begin() + left, A.begin() + mid);
-    vector<int> r(A.begin() + mid, A.begin() + right);
-
-    // Recursively sort the subarrays.
-    mergeSort(A, l, left, mid);
-    mergeSort(A, r, mid, right);
-
-    // Merge the sorted subarrays l and r into B.
-    merge(l, r, B);
+void mergeSort(vector<int>& arr, int l, int r){
+    if(l==r)return;
+    int mid = (l+r)/2;
+    mergeSort(arr, l, mid);
+    mergeSort(arr, mid+1, r);
+    merge(arr, l, mid, r);
 }
 
 int main() {
@@ -62,7 +45,6 @@ int main() {
     cin >> size;
 
     vector<int> arr(size);
-    vector<int> C(size);
 
     // Input elements of the array.
     for (int i = 0; i < size; i++) {
@@ -80,13 +62,13 @@ int main() {
     cout << endl;
 
     // Perform merge sort on the input array and store the sorted result in C.
-    mergeSort(arr, C, 0, size);
+    mergeSort(arr, 0, size-1);
 
     cout << "Array after Merge Sort" << endl;
 
     // Display the sorted array.
     for (int i = 0; i < size; i++) {
-        cout << C[i] << "\t";
+        cout << arr[i] << "\t";
     }
 
     cout << endl;
